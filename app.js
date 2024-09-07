48e95737a2bcd28f476ad06d93aab3295e314026
Adler Santos <adler.coelho.santos@icloud.com>
2024-09-06 21:24:07 -0300

    finalização do projeto de imersão

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa")
+    let section = document.getElementById
+    ("resultados-pesquisa")
     console.log(section);
     
-    let resultados = ""
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+    if(!campoPesquisa){
+        section.innerHTML ="Digite algo no campo de Pesquisa!"
+        return
+    }
+
+    console.log(campoPesquisa);
     
+    campoPesquisa = campoPesquisa.toLowerCase()
+        
+    let resultados = "";
+    let titulo = "";
+    let descricao ="";
+    let tags ="";
     
     for (let dado of dados) {
-        resultados += `
-        <div class="item-resultado">
-            <h2>
-                <a href=${dado.link} target="_blank">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}.</p>
-        </div>
-        `
-    }
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLocaleLowerCase() 
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href=${dado.link} target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}.</p>
+            </div>
+            `;
+        }
+     }
+      
+     if (!resultados) {
+        resultados = "Nada foi encontrado!!!"
+     }
     
     section.innerHTML = resultados
 }
--- dados.js
+++ dados.js
@@ -2,32 +2,38 @@ let dados =[
     {
     titulo: "Ipê Amarelo",
     descricao:"O Ipê-Amarelo: Um Tesouro da Flora Brasileira. O ipê-amarelo é uma das árvores mais admiradas e emblemáticas do Brasil. Sua beleza exuberante, especialmente durante a floração, a tornou um símbolo nacional e uma espécie extremamente valorizada em paisagismo e projetos de arborização urbana.Nome científico: O ipê-amarelo abrange diversas espécies do gênero Handroanthus, cada uma com suas particularidades. A mais conhecida é o Handroa nthus albus.A característica mais marcante do ipê-amarelo é sua floração exuberante. As flores amarelas intensas cobrem completamente os galhos, criando um espetáculo visual único. A época da floração varia de acordo com a região, mas geralmente ocorre no final do inverno ou início da primavera. Pode atingir grandes dimensões, chegando a 30 metros de altura e um tronco imponente.",
-    link: "https://pt.wikipedia.org/wiki/Ip%C3%AA-amarelo-flor-de-algod%C3%A3o"
+    link: "https://pt.wikipedia.org/wiki/Ip%C3%AA-amarelo-flor-de-algod%C3%A3o",
+    tags: "amarelo amarela árvore"
     },
     {
        titulo: "Vitória-Régia", 
        descricao: "A Rainha dos Lagos: A Vitória-Régia. A vitória-régia é uma das maiores plantas aquáticas do mundo e um dos símbolos mais icônicos da Amazônia. Suas folhas gigantescas, que podem chegar a dois metros de diâmetro, e suas flores brancas e rosadas a tornam uma verdadeira maravilha da natureza. É nativa da Amazônia e habita lagos e rios de águas calmas. Suas folhas são circulares e possuem bordas levantadas, que impedem que a planta afunde. As flores da vitória-régia são grandes e perfumadas, abrindo-se à noite e fechando-se durante o dia. A vitória-régia desempenha um papel importante no ecossistema aquático, fornecendo abrigo e alimento para diversas espécies de animais. Além disso, suas raízes ajudam a estabilizar o solo e a filtrar a água",
-       link: "https://pt.wikipedia.org/wiki/Vitória-régia"
+       link: "https://pt.wikipedia.org/wiki/Vitória-régia",
+       tags: "vitoria regia agua água amazônia grande"
     },
     {
         titulo: "Manacá",
         descricao: "A Mágica das Cores: O Manacá. O manacá é uma árvore brasileira conhecida por suas flores que mudam de cor ao longo do tempo, passando do branco para o rosa e, por fim, para o roxo. Essa característica singular a torna uma das plantas mais admiradas do país. O manacá é uma árvore de porte médio, com copa arredondada e densa. É encontrada em diversas regiões do Brasil, adaptando-se a diferentes tipos de clima. Além de sua beleza ornamental, o manacá é utilizado em projetos de paisagismo e arborização urbana. Sua madeira é leve e macia, sendo utilizada na fabricação de móveis e objetos decorativos.",
-        link:"https://pt.wikipedia.org/wiki/Manacá"
+        link:"https://pt.wikipedia.org/wiki/Manacá",
+        tags: "manaca branco rosa"
     },
     {
         titulo: "Orquídeas",
         descricao: "A Beleza da Natureza em Detalhe: A Orquídea. As orquídeas são plantas conhecidas por suas flores exuberantes e variadas. Existem milhares de espécies diferentes, cada uma com suas características únicas. As orquídeas são encontradas em todos os continentes, exceto na Antártida. Elas podem ser epífitas, terrestres ou rupícolas, adaptando-se a diversos ambientes. O cultivo de orquídeas é um hobby muito popular. Existem diversas espécies adaptadas ao cultivo doméstico, que exigem cuidados especiais em relação à luminosidade, umidade e temperatura.",
-        link: "https://pt.wikipedia.org/wiki/Orquídea"
+        link: "https://pt.wikipedia.org/wiki/Orquídea",
+        tags: "orquidea orquídea exótica"
     },
     {
         titulo: "Jacarandá",
         descricao: "O Encanto Roxo: O Jacarandá. O jacarandá é uma árvore brasileira conhecida por suas flores roxas, que transformam as ruas em um verdadeiro espetáculo durante a primavera. Sua beleza e fragrância encantam a todos. O jacarandá é uma árvore de grande porte, com copa ampla e arredondada. É nativa do Brasil e é amplamente utilizada em projetos de paisagismo e arborização urbana. Além de sua beleza ornamental, a madeira do jacarandá é muito apreciada na fabricação de móveis e instrumentos musicais.",
-        link: "https://pt.wikipedia.org/wiki/Jacarandá"
+        link: "https://pt.wikipedia.org/wiki/Jacarandá",
+        tags: "roxa lilás lilas árvore jacaranda"
     },
     {
         titulo: "Bromélia",
         descricao: "A Elegante Brasileira: A Bromélia. As bromélias são plantas epífitas, conhecidas por suas folhas em forma de roseta que formam uma espécie de taça. Nativas das Américas, principalmente do Brasil, elas encantam com sua variedade de cores e formas. As bromélias são adaptadas para viver em ambientes úmidos e sombreados, como as matas tropicais. Muitas espécies possuem tricomas em suas folhas, que absorvem água e nutrientes do ar. Existem milhares de espécies de bromélias, desde as pequenas e delicadas até as grandes e exuberantes. Além de sua beleza ornamental, algumas espécies são utilizadas na medicina popular e na produção de fibras.",
-        link: "https://pt.wikipedia.org/wiki/Bromelia"
+        link: "https://pt.wikipedia.org/wiki/Bromelia",
+        tags: "bromelia elegante folha"
     }
     
 ];
